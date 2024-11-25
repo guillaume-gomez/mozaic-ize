974398b7ba32f5d2e0e55be604d3eff5ed794330
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-11-25 08:55:34 +0100

    setup to test the algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import imageTest from "/lifesaver_opaque.jpg";
+import InputFileWithPreview from "./components/InputFileWithPreview";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [image, setImage] = useState<HTMLImageElement>();
+  const imageRef = useRef<HTMLImageElement>();
+  const canvasRef = useRef<HTMLCanvasDocument>();
+
+
+  useEffect(() => {
+    if(canvasRef.current && imageRef.current) {
+      const context = canvasRef.current.getContext('2d');
+      // create backing canvas
+      canvasRef.current.width = imageRef.current.width;
+      canvasRef.current.height = imageRef.current.height;
+      // restore main canvas
+      context.drawImage(imageRef.current, 0,0);
+    }
+  }, [canvasRef, imageRef]);
+
+  function uploadImage(newImage: HTMLImageElement) {
+    setImage(newImage);
+  }
 
   return (
     <>
@@ -17,6 +38,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <img src={imageTest} ref={imageRef} alt="image test" />
+      <InputFileWithPreview onChange={uploadImage} value={image} />
+      <canvas ref={canvasRef} width={500} height={500}/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
