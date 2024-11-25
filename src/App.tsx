f6745323ec3a695c05e5992e32cbb3ab051c3d35
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-11-25 15:41:48 +0100

    start naive algo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,18 @@ import imageTest from "/lifesaver_opaque.jpg";
 import InputFileWithPreview from "./components/InputFileWithPreview";
 import './App.css';
 
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+interface TileData {
+  color: Color;
+  x: number;
+  y: number;
+}
+
 function App() {
   const [count, setCount] = useState(0);
   const [image, setImage] = useState<HTMLImageElement>();
@@ -15,13 +27,69 @@ function App() {
   useEffect(() => {
     if(canvasRef.current && imageRef.current) {
       const context = canvasRef.current.getContext('2d');
+      const {width, height} = imageRef.current
       // create backing canvas
-      canvasRef.current.width = imageRef.current.width;
-      canvasRef.current.height = imageRef.current.height;
+      canvasRef.current.width = width;
+      canvasRef.current.height = height;
       // restore main canvas
       context.drawImage(imageRef.current, 0,0);
+
+      const tileSize = 8;
+      const tilesData = getColorsImage(context, width, height, tileSize);
+      context.clearRect(0,0, width, height);
+
+      context.fillStyle = `rgb(0 0 0)`;
+      context.fillRect(0,0, width, height);
+
+      drawTiles(context, tilesData, tileSize - 2);
     }
   }, [canvasRef, imageRef]);
+
+  function getColorsImage(context: CanvasRenderingContext2D, width: number, height: number, tileSize: number) : TileData[] {
+    const tilesData: TileData[] = [];
+    for(let x = 0; x < width; x+= tileSize) {
+      for(let y = 0; y < height; y+= tileSize) {
+        tilesData.push({
+          color: interpolateArea(context, tileSize, x, y),
+          x,
+          y
+        }
+        );
+      }
+    }
+    return tilesData;
+  }
+
+  function drawTiles(context: CanvasRenderingContext2D, tilesData: TileData[], tileSize: number) {
+    tilesData.forEach(colorData => {
+        const { red, green, blue } = colorData.color;
+        const { x, y } = colorData;
+
+        context.fillStyle = `rgb(${Math.floor(red)} ${Math.floor(green)} ${Math.floor(blue)})`;
+        context.fillRect(x,y, tileSize, tileSize);
+    });
+  }
+
+  function interpolateArea(context: CanvasRenderingContext2D, tileSize: number, x: number, y: number) : Color {
+    const pixels = context.getImageData(x,y, tileSize, tileSize);
+    const { data } = pixels;
+    const numberOfPixels = tileSize * tileSize;
+    let red = 0;
+    let green = 0;
+    let blue = 0;
+
+
+    for (let i = 0; i < data.length; i += 4) {
+      red += data[i];
+      green += data[i + 1];
+      blue += data[i + 2];
+    }
+
+    return { red: (red/numberOfPixels), green: (green/numberOfPixels), blue: (blue/numberOfPixels) };
+  }
+
+
+
 
   function uploadImage(newImage: HTMLImageElement) {
     setImage(newImage);
@@ -39,8 +107,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <img src={imageTest} ref={imageRef} alt="image test" />
-      <InputFileWithPreview onChange={uploadImage} value={image} />
       <canvas ref={canvasRef} width={500} height={500}/>
+      <InputFileWithPreview onChange={uploadImage} value={image} />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
