3b311e6742bc5751130e600200965f7d8aaf2b95
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-12-20 16:09:39 +0100

    use instance mesh to render many tiles

--- src/components/ThreeJs/MozaicManager.tsx
+++ src/components/ThreeJs/MozaicManager.tsx
@@ -1,6 +1,6 @@
-import { useMemo } from "react";
+import { useMemo, useEffect, useRef } from "react";
 import { Box } from '@react-three/drei';
-import { BoxGeometry, MeshStandardMaterial } from "three";
+import { Object3D, InstancedMesh, BoxGeometry, MeshStandardMaterial, Color } from 'three';
 import Tile from "./Tile";
 
 interface MozaicManagerProps {
@@ -24,35 +24,48 @@ function MozaicManager({
   backgroundColor,
   tilesData}: MozaicManagerProps)
 {
+    const meshRef = useRef<InstancedMesh>(null);
     const geometry = useMemo(() => new BoxGeometry(widthTile, heightTile, 20), [widthTile, heightTile]);
-    const materials = useMemo(() => {
-        let materialsMap = new Map();
-        tilesData.forEach(({color}) => {
-            const meshStandardMaterial = new MeshStandardMaterial({
-                color,
-                emissive:"#212121",
-                roughness:0.478,
-                metalness:0.122
-            });
-            materialsMap.set(color, meshStandardMaterial);
+    const material = useMemo(() => {
+        return new MeshStandardMaterial({
+            color: "#FFFFFF",
+            emissive:"#212121",
+            roughness:0.478,
+            metalness:0.122
         });
-        return materialsMap;
-    },[tilesData]);
+    },[]);
 
-    console.log(materials)
+  useEffect(() => {
+    init();
+  }, [tilesData,  meshRef.current]);
+
+
+  function init() {
+    if(!meshRef.current) {
+        console.log("not loaded")
+    }
+    tilesData.map(({x, y, color}, index) => {
+      const object = new Object3D();
+      const colorThree = new Color(color);
+      object.position.set(x, -y, 0.1);
+      object.updateMatrix();
+      meshRef.current?.setColorAt(index, colorThree);
+      meshRef.current?.setMatrixAt(index, object.matrix);
+    })
+
+    meshRef.current!.instanceMatrix.needsUpdate = true;
+  }
+
 
     return (
-    <group scale={1/SCALE} position={[0,heightMozaic/SCALE/2,0]}>
+    <group scale={1/SCALE} position={[-widthMozaic/2/SCALE,heightMozaic/SCALE,0]}>
       {/*<Box args={[widthMozaic, heightMozaic, 10]} material-color={backgroundColor} />*/}
-      {tilesData.map(({x, y, color}, position) =>
-        <Tile
-            key={position}
-            position={[x - widthMozaic/2, -y +heightMozaic/2, 0.1 ]}
-            geometry={geometry}
-            material={materials.get(color)}
-            color={color}
-        />
-      )}
+          <instancedMesh
+              receiveShadow={true}
+              castShadow={true}
+              ref={meshRef}
+              args={[geometry, material, tilesData.length ]}
+            />
     </group>
     )
 }
