e5238a14355203b8bacef99f66ad22b4c10776d5
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-12-27 12:59:15 +0100

    add step param in range

--- src/components/Range.tsx
+++ src/components/Range.tsx
@@ -5,16 +5,18 @@ interface RangeProps {
   value: number;
   min: value;
   max:value
+  step?: number
   onChange: (newValue: number) => void;
 }
 
-function Range({label, onChange, value, min, max} : RangeProps) {
+function Range({label, onChange, value, min, max, step = 1} : RangeProps) {
   
   return (
     <div className="flex flex-col gap-3">
       <label>
         {label}
         <input
+          step={step}
           min={min}
           max={max}
           type="range"
