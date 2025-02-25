b7f3ac24fa52d37b54a3a073c46881d3bdbbaaa3
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-02-25 14:50:35 +0100

    install daisyui

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     checker({typescript: true}),
   ],
