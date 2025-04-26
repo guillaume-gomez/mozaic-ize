cfcd4a058f611b68ca35f39468f363aec13dffe7
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-04-26 18:33:05 +0200

    allow custom header

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,18 @@
 import { ReactNode } from "react";
 
 interface CardProps {
-  label: string;
+  label: ReactNode;
   children: ReactNode;
 }
 
 function Card({ label, children }: CardProps) {
   return (
-    <div className="card card-border bg-base-200">
+    <div className="card card-border bg-base-200 w-full h-full">
       <div className="card-body">
-        <h2 className="card-title">{label}</h2>
-        <div className="flex flex-col gap-3">
+        <h2 className="card-title">
+          {label}
+        </h2>
+        <div className="flex flex-col gap-3 w-full h-full">
         {children}
         </div>
       </div>
