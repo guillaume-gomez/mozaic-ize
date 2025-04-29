94c0f4c160e67f8da2657cad029392f647e2b925
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-04-29 15:08:17 +0200

    change the design to make the website more modern

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import Select from "./components/Select";
 import ThreeJsRenderer from "./components/ThreeJs/ThreeJsRenderer";
 import { resizeImage } from "./utils";
 import useMozaic from "./components/Hooks/useMozaic";
-import Card from "./components/Card";
+import Toggle from "./components/Toggle";
+import Header from "./components/Header";
 
 function App() {
   const [originalImage, setOriginalImage] = useState<HTMLImageElement>();
@@ -19,6 +20,8 @@ function App() {
   const [artistName, setArtistName]= useState<string>("Made by Guillaume G");
   const [dataUrl, setDataUrl] = useState<string>("");
   const [isDirty, setIsDirty] = useState<boolean>(false);
+  // to conditionnaly hide some items
+  const [firstRender, setFirstRender] = useState<boolean>(true);
   const {
     tilesData,
     generateImage,
@@ -60,110 +63,123 @@ function App() {
       console.error("Cannot generate image");
       return;
     }
+
     setDataUrl(dataUrl);
     setIsDirty(false);
+    setFirstRender(false);
+    springApi.start();
   }
 
-  return (
-    <div className="container m-auto flex-col gap-5 lg:p-2 p-4">
-      <h1>Mosaic-ize</h1>
-      <div className="h-full grid grid-flow-col grid grid-cols-3 gap-4">
+  // pour le bouton, le faire passer de - sa position initiale à sa position finale
 
-        <Card
-          label={"Settings"}
-        >
-          <ColorPicker
-            label={"BackgroundColor"}
-            onChange={(color) => {
-              setBackgroundColor(color);
-              setIsDirty(true);
-            }}
-            value={backgroundColor}
-          />
-          <Range
-            min={16}
-            max={128}
-            step={8}
-            label={"Mozaic Tile"}
-            value={tileSize}
-            onChange={(value) => {
-              setTileSize(value);
-              setIsDirty(true);
-            }}
-          />
-          <Range
-            min={0}
-            max={tileSize - 20}
-            step={2}
-            label={"Padding"}
-            value={padding}
-            onChange={(value) => {
-              setPadding(value)
-              setIsDirty(true);
-            }}
-          />
-          <InputFileWithPreview onChange={uploadImage} value={image} />
-          <Select
-            label="Mode of generation"
-            value={imageColorMode}
-            onChange={(imageColorMode) => {
-              setImageColorMode(imageColorMode)
-              setIsDirty(true);
-            }}
-            options={[
-              {label: "Normal", value: "normal"},
-              {label: "Random", value: "random"},
-            ]}
-          />
-          <div className="form-control">
-            <label>Artist name</label>
-            <input
-              type="text"
-              className="input input-primary w-full" 
-              max={32}
-              value={artistName}
-              onChange={(e) => {
-                  if(e.target.value.length < 32) {
-                    setArtistName(e.target.value)
+  return (
+    <div className="bg-base-300">
+      <div className="relative container m-auto flex-col h-screen gap-5 lg:p-2 p-4">
+        <img src={dataUrl} className="hidden" />
+        <Header />
+        <div className="h-full flex md:flex-row flex-col gap-4 flex-grow">
+          <div className="lg:basis-4/12 md:basis-5/12 basis-auto content-center">
+
+              <div className="flex flex-col gap-8 p-4">
+                <div className="">
+                  <h2 className="md:text-6xl text-3xl font-extrabold">
+                    <p>
+                      Change
+                     <span className="px-2 line-through decoration-accent">photo</span>
+                    </p>
+                    walls into
+                    <span className="px-2 underline decoration-primary">art</span>
+                  </h2>
+                </div>
+                <div className="flex flex-col gap-3">
+                  <div>
+                    <label>Upload an image for start</label>
+                    <InputFileWithPreview onChange={uploadImage} value={image} />
+                  </div>
+                  {!firstRender &&
+                  <animated.div style={props}>
+                    <div className="form-control">
+                      <label>Sign you artwork (max 32 caracters)</label>
+                      <input
+                        type="text"
+                        className="input input-primary w-full" 
+                        max={32}
+                        value={artistName}
+                        onChange={(e) => {
+                            if(e.target.value.length < 32) {
+                              setArtistName(e.target.value)
+                            }
+                          }
+                        }
+                      />
+                    </div>
+                    <Range
+                      min={16}
+                      max={128}
+                      step={8}
+                      label={"Change the mozaic tile size"}
+                      value={tileSize}
+                      onChange={(value) => {
+                        setTileSize(value);
+                        setIsDirty(true);
+                      }}
+                    />
+                    <Range
+                      min={0}
+                      max={tileSize - 20}
+                      step={2}
+                      label={"Add or Reduce padding between the tiles"}
+                      value={padding}
+                      onChange={(value) => {
+                        setPadding(value)
+                        setIsDirty(true);
+                      }}
+                    />
+                    <ColorPicker
+                      label={"BackgroundColor"}
+                      onChange={(color) => {
+                        setBackgroundColor(color);
+                        setIsDirty(true);
+                      }}
+                      value={backgroundColor}
+                    />
+                    <Select
+                      label="Mode of generation"
+                      value={imageColorMode}
+                      onChange={(imageColorMode) => {
+                        setImageColorMode(imageColorMode)
+                        setIsDirty(true);
+                      }}
+                      options={[
+                        {label: "Normal", value: "normal"},
+                        {label: "Random", value: "random"},
+                      ]}
+                    />
+                    <Toggle
+                      label="See the plain mozaic image (2d version)"
+                      value={twoDimension}
+                      toggle={() => setTwoDimension(!twoDimension)}
+                    />
+                  </animated.div>
                   }
-                }
-              }
-            />
-          </div>
-          <button
-            className="btn btn-primary"
-            onClick={generate}
-            disabled={!isDirty}
-          >
-            Generate
-          </button>
-        </Card>
-        <div className="col-span-3 col-start-2">
-          <img src={dataUrl} className="hidden" />
-          <Card 
-            label={
-              <div role="tablist"
-                className="tabs tabs-box"
-              >
-                <a 
-                  role="tab"
-                  className={`tab ${!twoDimension ? "tab-active" : ""}`}
-                  onClick={() => setTwoDimension(false)}
-                >
-                  3D
-                </a>
-                <a 
-                  role="tab"
-                  className={`tab ${twoDimension ? "tab-active" : ""}`}
-                  onClick={() => setTwoDimension(true)}
-                >
-                  2D
-                </a>
+                  { image && 
+                    <div className="form-control flex flex-col gap-1">
+                      <label className="text-success">{ firstRender ? "Great! Then press generate to make some magic" : "Play around with the settings now !"}</label>
+                      <button
+                        className="btn btn-primary font-extrabold uppercase rounded-xl btn-lg"
+                        onClick={generate}
+                        disabled={!isDirty}
+                      >
+                        Generate
+                      </button>
+                    </div>
+                  }
+                </div>
               </div>
-            }
-          >
-            
-            {!twoDimension &&  dataUrl !== "" &&
+
+          </div>
+          <div className="lg:basis-8/12 md:basis-7/12 basis-auto bg-gradient-to-b from-sky-100 to-sky-500 rounded-xl">
+             {  !twoDimension &&
                 <ThreeJsRenderer
                   widthMozaic={width}
                   heightMozaic={height}
