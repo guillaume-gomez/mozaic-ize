00f597a0006772db7e6cf78e492c3adde25831ad
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-05-23 10:14:19 +0200

    update toggle designé

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -6,7 +6,7 @@ interface ToggleProps {
 
 function Toggle({ label, value, toggle } : ToggleProps) {
   return (
-    <div className="form-control">
+    <div className="flex flex-row gap-2">
         <span className="label-text">{label}</span>
         <input type="checkbox" className="toggle toggle-primary" checked={value} onChange={toggle} />
     </div>
